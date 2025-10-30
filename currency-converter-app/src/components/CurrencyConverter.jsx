e31dba7d85f8537974f0ebf2464241938a7d7614
LiebgottLiu <2402337165@qq.com>
2025-10-30 23:54:45 +1100

    adding docker file

--- currency-converter-app/src/components/CurrencyConverter.jsx
+++ currency-converter-app/src/components/CurrencyConverter.jsx
@@ -51,6 +51,7 @@ export default function CurrencyConverter() {
           onChange={setAudAmount}
           onConvert={fetchRates}
         />
+        
         <div>
           {currencies.map((cur) => (
             <CurrencyCard
