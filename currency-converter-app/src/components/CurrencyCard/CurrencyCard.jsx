2db5cfe6a4279a1410984b70415fd016cab0c327
LiebgottLiu <2402337165@qq.com>
2025-10-31 01:12:44 +1100

    Solve the problem of long loading delay

--- currency-converter-app/src/components/CurrencyCard/CurrencyCard.jsx
+++ currency-converter-app/src/components/CurrencyCard/CurrencyCard.jsx
@@ -4,6 +4,10 @@ import axios from "axios";
 import moment from "moment";
 import { getHistoricalRatesURL } from '../../config/api';
 import CurrencyModal from "../CurrencyGraphModal/CurrencyModal";
+
+// cache for historical data 
+const historicalCache = {};
+
 export default function CurrencyCard({ code, symbol, flag, amount, rate, lastUpdated  }) {
 
   const [showModal, setShowModal] = useState(false);
@@ -12,33 +16,43 @@ export default function CurrencyCard({ code, symbol, flag, amount, rate, lastUpd
 
   const handleClick = async () => {
     setShowModal(true);
-    setLoading(true);
+    
+    //use if it is in cache
+    if(historicalCache[code]){
+      setHistoricalData(historicalCache[code])
+    }
 
+    
 
     const baseDate = lastUpdated
     ? moment(lastUpdated, "DD/MM/YYYY HH:mm:ss")
     : moment();
 
-    const dataPoints = [];
+    setLoading(true);
 
-    for(let i = 13; i >= 0; i--){
-      const date =  baseDate.clone().subtract(i, 'days').format('YYYY-MM-DD');
+    const dates = Array.from({ length: 14 }, (_, i) =>
+      baseDate.clone().subtract(14 - i, "days").format("YYYY-MM-DD")
+    );
       
       try{
-        const response = await axios.get(getHistoricalRatesURL(date))
-        const rates = response.data.rates
-        const rateAUD = rates['AUD'];
-        const historicalRate = rateAUD && rates[code] ? (rates[code] / rateAUD).toFixed(4) : null;
-        dataPoints.push({
-          date,
-          rate:historicalRate,
-        });
+        const response = await Promise.all(
+          dates.map((date) => axios.get(getHistoricalRatesURL(date)))
+        ) 
+        const dataPoints = response.map((res,idx) =>{
+          const rates = res.data.rates;
+          const rateAUD = rates["AUD"];
+          const historicalRate = 
+              rateAUD && rates[code] ? (rates[code]/ rateAUD).toFixed(4): null;
+              return{ date:dates[idx],rate:historicalRate};
+        })
+        
+        historicalCache[code] = dataPoints;
+        setHistoricalData(dataPoints);
       }catch(e){
         console.error("Can not get history data" , e)
       }
-    }
+    
 
-    setHistoricalData(dataPoints);
     setLoading(false);
 
   };
@@ -71,6 +85,7 @@ export default function CurrencyCard({ code, symbol, flag, amount, rate, lastUpd
         onClose={() => setShowModal(false)}
         data={historicalData}
         code={code}
+        loading = {loading}
       />
     </>
   );
--- currency-converter-app/src/components/CurrencyGraphModal/CurrencyModal.jsx
+++ currency-converter-app/src/components/CurrencyGraphModal/CurrencyModal.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 import "./index.css";
 
-export default function CurrencyModal({ visible, onClose, data, code }) {
+export default function CurrencyModal({ visible, onClose, data, code, loading }) {
   if (!visible) return null;
 
   return (
@@ -10,28 +10,36 @@ export default function CurrencyModal({ visible, onClose, data, code }) {
       <div className="currency-modal-content" onClick={(e) => e.stopPropagation()}>
         <h3 className="text-xl font-semibold mb-4">AUD to {code} - Last 14 Days</h3>
 
-        <ResponsiveContainer width="100%" height={400}>
-          <LineChart data={data}>
-            <CartesianGrid stroke="#ccc" />
-            <XAxis
-              dataKey="date"
-              ticks={data.filter((_, i) => i % 5 === 0).map(d => d.date)}
-              
-              textAnchor="star"  
-              height={60} 
-            />
-            <YAxis domain={['auto', 'auto']} />
-            <Tooltip />
-            <Line
-              type="monotone"
-              dataKey="rate"
-              stroke="#8884d8"
-              dot={{ r: 3 }}
-              activeDot={{ r: 8 }}
-            />
-          </LineChart>
-        </ResponsiveContainer>
 
+        {loading? (
+            <div className="loading-container">
+            <div className="spinner" />
+            <p>Loading...</p>
+          </div>
+        ): (
+            <ResponsiveContainer width="100%" height={400}>
+            <LineChart data={data}>
+                <CartesianGrid stroke="#ccc" />
+                <XAxis
+                dataKey="date"
+                ticks={data.filter((_, i) => i % 5 === 0).map(d => d.date)}
+                
+                textAnchor="star"  
+                height={60} 
+                />
+                <YAxis domain={['auto', 'auto']} />
+                <Tooltip />
+                <Line
+                type="monotone"
+                dataKey="rate"
+                stroke="#8884d8"
+                dot={{ r: 3 }}
+                activeDot={{ r: 8 }}
+                />
+            </LineChart>
+            </ResponsiveContainer>
+        )}
+        
         <button className="currency-modal-close" onClick={onClose}>✕</button>
       </div>
     </div>
